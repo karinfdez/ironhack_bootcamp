884d1790308cae1fba2c956be3fabb3f0f86af01
Karin Fernandez <karinfdez@yahoo.com>
2015-10-25 13:19:10 -0400

    how to count same products?

---
+++ week_1/shoppingCart.rb
@@ -0,0 +1,92 @@
+require "highline/import"  #Prompt user and get his/hers input
+
+class Season_List
+
+	attr_accessor :productsMarket
+	def initialize
+		@productsMarket={}
+	end
+
+	def season_input
+		picked_season=ask "Please enter the actual season to display product's prices:\nYou can type 'Spring', 'Summer', 'Autumn' and 'Winter':"
+		picked_season.chomp.downcase
+		
+	  puts case picked_season
+			when "spring"
+				productsMarket={
+			    	apples: 10, 
+			    	banana: 20,
+			    	oranges: 5,
+			    	grapes: 15,
+			    }
+			when "summer"
+				productsMarket={
+				    apples: 10, 
+				    banana: 20,
+				    oranges: 2,
+				    grapes: 15,
+			    }
+			
+			when "autumn"
+			  productsMarket={
+				    apples: 15, 
+				    banana: 20,
+				    oranges: 5,
+				    grapes: 15,
+			    }  
+			when "winter"
+				productsMarket={
+				    apples: 12, 
+				    banana: 21,
+				    oranges: 5,
+				    grapes: 15,
+			    }  
+
+			else
+			  "Thats not the correct season. Try again"
+			end
+		productsMarket
+	end
+
+end
+
+class ShoppingCart 
+
+	attr_accessor :productsMarket, :list_products
+
+	def initialize(product_list)
+
+	    @productsMarket=product_list
+	    @list_products={}
+    end
+	
+	def add_item_to_cart product_id      #Add the cost of the selected products
+		
+		list_products[product_id]=productsMarket[product_id]
+	end
+
+	#Show shopping cart list
+	def show
+		puts "\nShopping list: "
+        list_products.each {|key, val| print key, " ", val, "$\n"}     
+	end
+  
+    #Calculate the total cost of shopping cart
+    def cost 
+ 
+        sum=0
+    	list_products.each {|key,val| sum+= val}
+    		puts "\nTotal cost: #{sum}$"
+    end
+end  
+
+
+list_season=Season_List.new
+list=list_season.season_input
+cart = ShoppingCart.new(list)
+cart.add_item_to_cart :apples
+cart.add_item_to_cart :banana
+cart.add_item_to_cart :banana
+cart.show
+cart.cost
+

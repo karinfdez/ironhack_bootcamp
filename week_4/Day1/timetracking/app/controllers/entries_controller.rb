dc23d9873bd3f0ec75f46cce4de79d3113860338
Karin Fernandez <karinfdez@yahoo.com>
2015-11-11 18:21:42 -0500

    Show the errors on the form page

--- week_4/Day1/timetracking/app/controllers/entries_controller.rb
+++ week_4/Day1/timetracking/app/controllers/entries_controller.rb
@@ -18,4 +18,26 @@ class EntriesController < ApplicationController
 		@entry=@proj.entries.new
 		render("new")
 	end
+
+	def create
+		id=params[:project_id]
+		@proj=Project.find_by(id: id)
+		@entr=@proj.entries.new(entry_params)   #method passed as parameter)
+		if @entr.save
+			redirect_to action: "index",controller: "entries", project_id: @proj.id
+			# redirect_to("/projects/#{@proj.id}/entries")
+	   else
+	   	 render 'new'  #Send me to the form again
+	   end
+
+		#show the new page
+		#show errors
+	end
+
+	private
+	#Always rails reuqire this method to avoid malicious input
+	def entry_params
+		params.require(:entry).permit(:hours,:minutes,:date)
+	end
+
 end

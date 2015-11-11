49360157fbb058319ce079dd6c5e2df16f792468
Karin Fernandez <karinfdez@yahoo.com>
2015-11-10 22:11:20 -0500

    until favorite contact

--- week_4/Day1/address_book/app/controllers/contacts_controller.rb
+++ week_4/Day1/address_book/app/controllers/contacts_controller.rb
@@ -1,6 +1,8 @@
 class ContactsController < ApplicationController
 	def index
-		 @contacts=Contact.all  #Shows me all contacts
+		
+		 @contacts=Contact.order('name ASC')
+		 # @contacts=Contact.all  #Shows me all contacts
 		 render 'index'
 	end
 
@@ -9,8 +11,66 @@ class ContactsController < ApplicationController
 	end
 
 	def create
-		render(:text => "This route is contacts#create.")
+		# render(:text => "This route is contacts#create.")
+    # render(:text => params.inspect)   #Show me how is the information is saved on contact. This a hash with a contact inside, and have hashes inside
+	# render(:text => params[:contact].inspect)   #Here I just showing what I create. The keys that are inside contact.
+	# render(:text => params[:contact][:name]) #Just print
+		@contact = Contact.new(
+	      :name => params[:contact][:name],
+	      :address => params[:contact][:address],
+	      :phone_number => params[:contact][:phone_number],
+	      :email_address => params[:contact][:email_address])
+		
+		# puts "ASDFDFGFGDFGDFGDFGDFGDFGDFGD================"
+		# p contact.name
+		# p contact.address
+		if !(@contact.name=="" && @contact.address=="") #VALIDATION
+			@contact.save
+			redirect_to("/")   #To all the lists of contacts, included the new one that I created
+		
+		    # render(:text => contact.attributes)
+		    # render(:text => "save")
+		else
+			# @contact
+		    render(:text => "Fill the name or address fields")
+		end
+		
 	end
 
+	def show
+
+		id=params[:id]					#To show the user the id of contact on the url
+		@contacts=Contact.find_by(id: id)     #From here I should go and create a show.html.erb on views folder
+		if @contacts==nil
+			redirect_to("/404")
+		else
+			render 'show'
+		end
+	end
+
+	def favorite
+		id=params[:id]	
+		@contacts=Contact.find_by(id: id)
+		# @contacts.favorite=true   #Change the state of favorites to true
+		# @contacts.save
+		#better way and straight to do it:
+		@contacts.update(favorite: true)   #Change favorites to true and save it
+		redirect_to("/contacts/favorites") #Redirecting from this action to showfavorite action and the showfavorite.html.erb
+	end                                    #There is where I use the get("/projects/favorites")
+
+	def showfavorites
+		# id=params[:id]					#To show the user the id of contact on the url
+		@contacts=Contact.where(favorite: true)     #From here I should go and create a show.html.erb on views folder
+		if @contacts==nil
+			redirect_to("/404")
+		else
+			render 'showfavorites'
+		end
+	end
+
+	# def findcontact
+	# 	name=params[:name]
+	# 	@contacts=Contact.where(name: )
+	# end
 
 end
--- week_4/Day1/address_book/config/routes.rb
+++ week_4/Day1/address_book/config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   get '/'=>'contacts#index'
   get '/contacts/new'=>'contacts#new'   #TO show the form
   post("/contacts", :to => "contacts#create")   #to go to de action 'create'.
-end
+  get '/projects/:id' => 'contacts#show'     #This is the first step, then I should go to to constacs_controller
+  											# and create my show method
+  post("/contacts/:id/favorite", :to => "contacts#favorite") #To go to the action "favorite where I save the favorites contacts"
+  get "/contacts/favorites" =>'contacts#showfavorites'        #To show the favorites contacts
+
+#I got this one from the the form that has the same action name
+  # post("/contacts/<%= @contacts.id %>/byfirstletter", :to => "contacts#findcontact") #To go to the action "favorite where I save the favorites contacts"
+#Now I have to go to findcontact action on the controller   
+
+end                                                           #Get those on my showfavorites method

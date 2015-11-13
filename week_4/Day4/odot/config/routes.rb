5d7a679e8eeb96a2e1aa86e405c116876493b94a
Karin Fernandez <karinfdez@yahoo.com>
2015-11-12 20:42:52 -0500

    generate todo_list scaffold

--- week_4/Day4/odot/config/routes.rb
+++ week_4/Day4/odot/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :todo_lists
+
+  root 'todo_lists#index'  #Now todo_lists is show on local3000.
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

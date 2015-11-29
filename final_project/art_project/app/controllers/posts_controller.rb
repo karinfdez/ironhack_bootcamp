616253d6aef38cd7b2239570f320ecd44f9336e7
Karin Fernandez <karinfdez@yahoo.com>
2015-11-29 14:52:54 -0500

    List post with Show, Edit and Destroy links

--- final_project/art_project/app/controllers/posts_controller.rb
+++ final_project/art_project/app/controllers/posts_controller.rb
@@ -1,8 +1,16 @@
 class PostsController < ApplicationController
 	
 	def index
-		@list_Posts=Post.all
-	end
+		#Show all posts by descendant order(last, shows first)
+		@list_Posts= Post.order("id DESC").all
+
+ 		respond_to do |format|
+    		format.html  # index.html.erb
+    		format.json  { render :json => @list_Posts }
+    	end
+    end
+
+	
 	def new
 		@post=Post.new
 	end
@@ -21,15 +29,47 @@ class PostsController < ApplicationController
 	end
 	
 	def show
+		id=params[:id]
+		@post=Post.find_by(id: id)
+		if @post==nil
+			redirect_to('/404')
+		else
+			render 'show'
+		end
+
+	end
+
+	def edit
+		@post=Post.find_by(id: params[:id])
+		if @post==nil
+			redirect_to('/404')
+		else
+   		  # if @post.update(post_params)
+      	# 	 redirect_to posts_path
+    	  # else
+      	# 	render 'edit'
+    	  # end
+    	respond_to do |format|
+      		if @post.update(post_params)
+        		format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        		format.json { render :show, status: :ok, location: @post }
+      		else
+        		format.html { render :edit }
+        		format.json { render json: @post.errors, status: :unprocessable_entity }
+            end
+        end
+       end	
+       
 	end
 
 	def delete
 		@post.destroy
-		
+
 	end
 
 	private
+	
 	def post_params
-		params.require(:post).permit(:title,:content)
+		params.require(:post).permit(:title, :content)
 	end
 end
--- final_project/art_project/app/models/post.rb
+++ final_project/art_project/app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
 
+	
 	#title and content can't be blank. Length minimum 5 character for title.
 	validates :title, presence:true 
 	validates :title, length:{ minimum: 2 }
+	validates :title, uniqueness: true
     validates :content, presence: true
     validates :content, length: {minimum: 10}
 end

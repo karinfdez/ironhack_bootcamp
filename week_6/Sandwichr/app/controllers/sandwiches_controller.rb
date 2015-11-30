723e91826cee4f036e965fa0835cffcafaa6ad54
Karin Fernandez <karinfdez@yahoo.com>
2015-11-30 11:57:27 -0500

    Show action on API and error

--- week_6/Sandwichr/app/controllers/sandwiches_controller.rb
+++ week_6/Sandwichr/app/controllers/sandwiches_controller.rb
@@ -1,2 +1,26 @@
 class SandwichesController < ApplicationController
+		def index
+		sandwiches=Sandwich.all
+		render json: sandwiches
+		end
+
+		def create
+			sandwich=Sandwich.create(sandwich_params)
+			render json: sandwich
+		end
+		def show
+			my_sandwich= Sandwich.find_by(id: params[:id])
+			if my_sandwich.nil?
+				error={ error: "Sandwich not found"}
+				render json: error, status: 404
+			else
+				render json: my_sandwich
+			end
+		end
+
+
+		private
+		def sandwich_params
+			params.require(:sandwich).permit(:name,:bread_type)
+		end
 end

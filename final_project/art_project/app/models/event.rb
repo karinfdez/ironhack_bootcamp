1e778fc923a1716d3afba8bcc27a0312b27c5b7b
Karin Fernandez <karinfdez@yahoo.com>
2015-12-02 12:46:52 -0500

    Authentication at log in and image uploader

--- final_project/art_project/app/models/event.rb
+++ final_project/art_project/app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
 	#Events validations
 	validates :title,:location,:description, :organizer_name,:event_type,presence: true
 	validates :title,:organizer_name,length: { minimum: 1 }
+
+  #Includes the ImageUploader class
+	mount_uploader :image,ImageUploader
 end

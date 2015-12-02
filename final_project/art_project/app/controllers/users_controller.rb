a627cd6c027e841889aab9ccc0eeaf26183614fa
Karin Fernandez <karinfdez@yahoo.com>
2015-12-02 15:02:10 -0500

    Added link on signed user to the list of users

--- final_project/art_project/app/controllers/users_controller.rb
+++ final_project/art_project/app/controllers/users_controller.rb
@@ -1,25 +1,34 @@
 class UsersController < ApplicationController
 
-before_action :authenticate_user!
- def index
- 	@users=User.all
- end
+	before_action :authenticate_user!
+	 def index
+	 	@users=User.all
+	 end
 
- def show
- 	user=User.find_by(id: params[:id])
- 	render 'show'
- end
+	def new
+		@user=User.new
+	end
+	def create
+		@user = User.new(user_params)
 
- def edit
- end
+	    respond_to do |format|
+	      if @user.save
+	        format.html { redirect_to @user, notice: 'User was successfully created.' }
+	        format.json { render :show, status: :created, location: @user }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end
 
- def destroy
- 	user=User.find_by(id: params[:id])
- 	user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+	 def destroy
+	 	user=User.find_by(id: params[:id])
+	 	user.destroy
+	    respond_to do |format|
+	      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+	      format.json { head :no_content }
+	    end
  end
 
 end

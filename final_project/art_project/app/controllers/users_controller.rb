d8bd618e456109ea3eaf282178645ce71a13c3c9
Karin Fernandez <karinfdez@yahoo.com>
2015-12-02 14:34:56 -0500

    Added destroy action for user list

--- final_project/art_project/app/controllers/users_controller.rb
+++ final_project/art_project/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
 
+before_action :authenticate_user!
  def index
  	@users=User.all
  end
+
+ def show
+ 	user=User.find_by(id: params[:id])
+ 	render 'show'
+ end
+
+ def edit
+ end
+
+ def destroy
+ 	user=User.find_by(id: params[:id])
+ 	user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+ end
+
 end
 

fa3b4be8b27b1b81d50f8532f7a467fce6d66900
Karin Fernandez <karinfdez@yahoo.com>
2015-12-02 16:59:29 -0500

    Modifications on user's controller

--- final_project/art_project/app/controllers/users_controller.rb
+++ final_project/art_project/app/controllers/users_controller.rb
@@ -1,35 +1,45 @@
 class UsersController < ApplicationController
 
-	before_action :authenticate_user!
+	#Nobody(except the loged user) can have access to the list of users
+	# before_action :authenticate_user!
+
 	 def index
 	 	@users=User.all
 	 end
 
-	def new
-		@user=User.new
-	end
-	def create
-		@user = User.new(user_params)
 
-	    respond_to do |format|
-	      if @user.save
-	        format.html { redirect_to @user, notice: 'User was successfully created.' }
-	        format.json { render :show, status: :created, location: @user }
-	      else
-	        format.html { render :new }
-	        format.json { render json: @user.errors, status: :unprocessable_entity }
-	      end
-	    end
-	end
+	#Create and New action for admin's controller
+	# def new
+	# 	@user=User.new
+	# end
 
-	 def destroy
-	 	user=User.find_by(id: params[:id])
-	 	user.destroy
-	    respond_to do |format|
-	      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-	      format.json { head :no_content }
-	    end
- end
+	# def create
+	# 	@user = User.new(user_params)
+
+	#     respond_to do |format|
+	#       if @user.save
+	#         format.html { redirect_to @user, notice: 'User was successfully created.' }
+	#         format.json { render :show, status: :created, location: @user }
+	#       else
+	#         format.html { render :new }
+	#         format.json { render json: @user.errors, status: :unprocessable_entity }
+	#       end
+	#     end
+	# end
+
+	# def show
+	# 	@user=User.find_by(id: params[:id])
+	# end
+
+	#I'm going to use this part on the admin's controller
+	#  def destroy
+	#  	user=User.find_by(id: params[:id])
+	#  	user.destroy
+	#     respond_to do |format|
+	#       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+	#       format.json { head :no_content }
+	#     end
+ # end
 
 end
 
--- final_project/art_project/app/models/post.rb
+++ final_project/art_project/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
 
 mount_uploader :image,ImagePostUploader
 belongs_to :user
+
+validates :title,:author,:content, presence: true
+validates :title, length: { in: 2..80 },uniqueness: true
+
 end

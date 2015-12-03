a8aa842b8ba03fbd973f01c56324159a1259b198
Karin Fernandez <karinfdez@yahoo.com>
2015-12-03 13:38:29 -0500

    Using gem CanCan for user's authorization

--- final_project/art_project/app/controllers/posts_controller.rb
+++ final_project/art_project/app/controllers/posts_controller.rb
@@ -1,42 +1,46 @@
 class PostsController < ApplicationController
   
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!,except: [:index]
 
   #This step makes sure every post belongs to an specific user.
   #Loading the parent
-  before_filter(:find_user)
-
+  before_action(:find_user)
 
   # GET /posts
   # GET /posts.json
   #Only show posts for the specific user
   def index
-    @posts = @user.posts.find(:all)
+    # @post=set_post
+     @posts = current_user.posts.all
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @user=find_user
+    @post=@user.posts.find_by(id: params[:user_id])
   end
 
   # GET /posts/new
   def new
-
-    @post = @user.posts.new
+    @post = current_user.posts.new
   end
 
   # GET /posts/1/edit
   def edit
+
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = @user.posts.new(post_params)
+    #Only the loged user can create posts for himself, not any other user.
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to ([@user,@post]), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -62,7 +66,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
+    current_user.posts.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
@@ -72,11 +76,12 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = @user.posts.find(:all)
+      user=find_user
+      @post = @user.posts.all
     end
 
     def find_user
-      @user = User.find(params[:user_id])
+      @user = current_user.find(params[:user_id])
    end
 
     # Never trust parameters from the scary internet, only allow the white list through.

fe1792b2fca08bccb8e3eea991a4132d9de413a1
Karin Fernandez <karinfdez@yahoo.com>
2015-12-05 18:21:02 -0500

    added image to user page

--- final_project/art_project/app/controllers/users_controller.rb
+++ final_project/art_project/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 #Throw an exception if unauthorized user is trying to access this pages.
   #Like an log in user but when not admin preferences.
  # load_and_authorize_resource
-  
+  before_filter :load_user
  
   #Authenticate user when trying to edit, create or modify users(if it's not log in).
   #Unauthorized users only can see artists and theirs blogs.
@@ -28,17 +28,34 @@ class UsersController < ApplicationController
 	end
 	
 	def create
-		@user = User.new(user_params)
+		# @user = User.new(user_params)
 
 	    respond_to do |format|
 	      if @user.save
-	        format.html { redirect_to '/users', notice: 'User was successfully created.' }
+	        format.html { redirect_to '/users/@user.id', notice: 'User was successfully created.' }
 	        format.json { render :show, status: :created, location: @user }
 	      else
 	        format.html { render :new }
 	        format.json { render json: @user.errors, status: :unprocessable_entity }
 	      end
 	    end
+	end
+
+	def edit
+
+  end
+
+  def update
+    respond_to do |format|
+       @user = User.find_by(id: params[:id])
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def show
@@ -56,13 +73,13 @@ class UsersController < ApplicationController
 	private
 	def load_user
             @user = User.find_by(id: params[:id])
-            if @user.nil?
-        		redirect_to '/404'
-            end
+          #   if @user.nil?
+        		# redirect_to '/404'
+          #   end
 	end
 
 	def user_params
-      params.require(:user).permit(:first_name, :last_name, :email,:users_description,:password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name,:image,:email,:users_description,:password, :password_confirmation)
 
     end
 end

05e62783c951799c798dd047aa4f60110590bc3b
Karin Fernandez <karinfdez@yahoo.com>
2015-12-21 19:07:52 -0500

    Added the exercise order

--- Coding_Challenges/typos.rb
+++ Coding_Challenges/typos.rb
@@ -1,4 +1,26 @@
 
+# Write a function in in Ruby that receives two strings as arguments. 
+# The function should figure out whether the second string is the same as the first,
+#  possibly with one typo.
+
+# Let's say your first string is pizza. If the second string is also pizza, return true.
+
+# If the second string is pzza, it's only missing the i. In that case it's off by one 
+# character so we should return true. Similarly, if the second string is missing a 
+# different character, return true. Examples would be: pizz, izza, piza, etc. 
+# If it's missing two or more characters (like with pzz), return false.
+
+# If the second string is pizzza, it has an extra z. In that case it's also off by one
+#  character, so we would return true. Similarly, if the second string has any 
+#  one additional character, return true. Examples would be: pizzaz, opizza, piuzza, etc. 
+#  If the second string has two or more additional characters, return false.
+
+# If the second string is puzza, it's got a u where an i should be. In that case it's also
+# off by one character, so we would return true. Similarly, if the second string has any 
+# only one character that is incorrenct, return true. Examples would be: oizza, pixza,
+# pizzs, etc. If the second string has two or more characters that are incorrect, **return 
+# false.
+
 def typo?(string1, string2)
 	
 	counter=0
@@ -13,6 +35,7 @@ def typo?(string1, string2)
 	end
 	 puts typo
 end
+
 
 typo?("cake", "bake") #=> true
 typo?("cake", "cakes") #=> true
